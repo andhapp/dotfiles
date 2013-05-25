9302df801e7894eb0d23b7ccfb09cbd6ff232e88
Anuj Dutta <anuj@andhapp.com>
2013-05-25 21:21:38 +0100

    Add some new git commands for ease.

--- bash/git.sh
+++ bash/git.sh
@@ -1,11 +1,36 @@
-function git_parse_branch() {
-  if [[ -d ".git" && ! -L ".git" ]] ; then
-    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+function current_git_branch {
+  if [[ -d ".git" && ! -L ".git" ]]; then
+    branch=`git rev-parse --abbrev-ref HEAD`
+    if [[ $branch != "" ]]; then
+      echo "($branch)"
+    fi
   fi
 }
 
-function git_commit_and_push() {
+function git_commit_and_push {
   gadd
   g c -am "$1"
   g ph
+}
+
+function git_history {
+  git log $filename | grep "Date: "
+}
+
+function number_of_commits {
+  git_history | wc -l
+}
+
+function git_repository_usage {
+  for filename in $(find app test lib -iname "*.rb"); do
+    echo "$filename `number_of_commits`" | xargs echo
+  done
+}
+
+function first_commit {
+  git_history | tail -1
+}
+
+function last_commit {
+  git_history | head -1
 }
